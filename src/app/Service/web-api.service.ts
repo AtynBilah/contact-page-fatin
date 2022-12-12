5fdc19353a38e79d0d14d97c9fa01a543d02cbb8
Atyn <fatin123nabilah@gmail.com>
2022-12-12 21:23:55 +0800

    Front end integration

--- src/app/Service/web-api.service.ts
+++ src/app/Service/web-api.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebApiService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  // Get call method
+  // Param 1 : url
+  get(url: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Cache-Control' : 'no-cache',
+        'Pragma' : 'no-cache'
+      }),
+      observe: "response" as 'body'
+    };
+    return this.httpClient.get(
+      url,
+      httpOptions
+    )
+      .pipe(
+        map((response: any) => this.ReturnResponseData(response)),
+        catchError(this.handleError)
+      );
+  }
+
+  // Post call method 
+  // Param 1 : url
+  // Param 2 : model
+  post(url: string, model: any): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }), 
+     observe: "response" as 'body'
+    };
+    return this.httpClient.post(
+      url,
+      model,
+      httpOptions)
+      .pipe(
+        map((response: any) => this.ReturnResponseData(response)),
+        catchError(this.handleError)
+      );
+  }
+
+  private ReturnResponseData(response: any) {
+    return response;
+  }
+  private handleError(error: any) {
+    return throwError(error);
+  }
+
+
 }
