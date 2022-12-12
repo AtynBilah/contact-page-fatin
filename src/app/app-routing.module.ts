5e2e961bea72b78058dea132eab5e76360326599
Atyn <fatin123nabilah@gmail.com>
2022-12-12 18:12:09 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Contacts', pathMatch: 'full'},
+  { path: 'Contacts', component: ContactsComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
